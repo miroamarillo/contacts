a252cb2bd53d7403ec7f63dcde006c5bd48ff74e
Julian Pineda <studio@miroamarillo.com>
2014-10-14 16:45:15 -0400

    wrapping angular inside a selfie

--- public/src/app.js
+++ public/src/app.js
@@ -3,11 +3,18 @@
 *
 * A simple contacts app
 */
+(function(){
+
+
 angular.module('ContactsApp', ['ngRoute'])
 	.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider) {
 			$routeProvider
 				.when('/contacts', {
 					controller: 'ListController',
 					templateUrl: 'views/list.html'
-				})
+				});
+			$locationProvider.html5Mode(true);
 	}])
+
+//End of the selfie function
+})();
