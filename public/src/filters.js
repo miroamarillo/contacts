bb67bf88324839955cf3652a573d1b230819f706
Julian Pineda <studio@miroamarillo.com>
2014-10-15 16:53:44 -0400

    Creating a custom filter

---
+++ public/src/filters.js
@@ -0,0 +1,9 @@
+(function () {
+	angular.module('ContactsApp')
+		.filter('labelCase', function(){
+			return function(input){
+				input = input.replace(/([A-Z])/g, ' $1');
+				return input[0].toUpperCase() + input.slice(1);
+			}
+		})
+})();
