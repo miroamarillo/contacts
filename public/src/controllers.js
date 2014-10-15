8e69feddbebf972db3cd9ebbc5531eeda033af7d
Julian Pineda <studio@miroamarillo.com>
2014-10-15 17:13:25 -0400

    adding the controller for the new contact page

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -10,8 +10,30 @@
 			}
 			$scope.sort.field = 'firstName';
 			$scope.sort.order = false;
+
 			$scope.show = function(id){
 				$location.url('/contact/' + id);
 			}
-		}]);
+		}])
+		.controller('NewController', ['$scope', 'Contact', '$location', function($scope, Contact, $location){
+			$scope.contact = new Contact({
+				firstName: ['', 'text'],
+				lastName: ['', 'text'],
+				email: ['', 'email'],
+				homePhone: ['', 'tel'],
+				cellPhone: ['', 'tel'],
+				birthday: ['', 'date'],
+				website: ['', 'url'],
+				address: ['', 'text']
+			});
+			$scope.save = function(){
+				if($scope.newContact.$invalid){
+					$scope.$broadcast('record:invalid');
+				}
+				else{
+					$scope.contact.$save();
+					$location.url('/contacts');
+				}
+			};
+		}])
 })();
