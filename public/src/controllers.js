741f08b78874eadfbd9a88d390b821e0e7c28fe9
Julian Pineda <studio@miroamarillo.com>
2014-10-15 16:53:06 -0400

    making ListController work to display the list

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -1,6 +1,17 @@
 (function () {
 	angular.module('ContactsApp')
-		.controller('ListController', ['$scope', function($scope){
-			$scope.contacts = [];
+		.controller('ListController', ['$scope','Contact', '$location', function($scope, Contact, $location){
+			$scope.contacts = Contact.query();
+			$scope.fields = ['firstName', 'lastName'];
+
+			$scope.sort = function(field){
+				$scope.sort.field = field;
+				$scope.sort.order = !$scope.sort.order;
+			}
+			$scope.sort.field = 'firstName';
+			$scope.sort.order = false;
+			$scope.show = function(id){
+				$location.url('/contact/' + id);
+			}
 		}]);
 })();
