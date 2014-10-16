4e044e409055f307f6699184ccda3210dcf72046
Julian Pineda <studio@miroamarillo.com>
2014-10-16 10:23:22 -0400

    cameCase filter intended to parse the new fields and store them in the data.json

--- public/src/filters.js
+++ public/src/filters.js
@@ -6,4 +6,22 @@
 				return input[0].toUpperCase() + input.slice(1);
 			}
 		})
+		.filter('camelCase', function(){
+			return function(input){
+				return input.toLowerCase().replace(/ (\w)/g, function(match, letter){
+					return letter.toUpperCase();
+				})
+			}
+		})
+		.filter('keyFilter', function(){
+			return function (obj, query){
+				var result = {};
+				angular.forEach(obj, function(value, key){
+					if(key !== query){
+						result[key] = value;
+					}
+				});
+				return result;
+			}
+		})
 })();
