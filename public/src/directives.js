ea28c20bd17ae27e1425c49fe93ffa779ac653d6
Julian Pineda <studio@miroamarillo.com>
2014-10-15 20:31:08 -0400

    Creating the directive formField

--- public/src/directives.js
+++ public/src/directives.js
@@ -1,4 +1,41 @@
 (function () {
 	angular.module('ContactsApp')
+		.directive('formField', ['$timeout', 'FieldTypes', function($timeout, FieldTypes){
+			// Runs during compile
+			return {
+				restrict: 'EA',
+				templateUrl: 'views/form-field.html',
+				replace: true,
+				scope: {
+					record: '=',
+					field: '@',
+					live: '@',
+					required: '@'
+				},
+				link: function($scope, element, attr) {
+					console.log("I'm in");
+					$scope.types = FieldTypes;
+
+					$scope.remove = function(field){
+						delete $scope.record[field];
+						$scope.blurUpdate();
+					}
+
+					$scope.blurUpdate = function(){
+						if($scope.live !== 'false'){
+							$scope.record.$update(function(updatedRecord){
+								$scope.record = updatedRecord;
+							});
+						}
+					}
+
+					var saveTimeout;
+					$scope.update = function(){
+						$timeout.cancel(saveTimeout);
+						saveTimeout = $timeout($scope.blurUpdate, 1000);
+					}
+				}
+			};
+		}])
 
 })();
