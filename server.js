afd5c32f06313857b6e926480742f5ffed455b1b
Julian Pineda <studio@miroamarillo.com>
2014-10-16 10:56:24 -0400

    correcting sendfile

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var express = require('express'),
 	api 	= require('./api'),
-	app 	= express();
+	app 	= express(),
+	//Require the path
+	path 	= require('path');
 
 app
 	.use(express.static('./public'))
 	.use('/api', api)
 	.get('*', function(req, res){
-		res.sendfile('public/main.html');
+		//With res.sendFile, you need to supply an absolute path:
+		res.sendFile( path.join(__dirname + '/public','main.html'));
+		//To use path.join you need to require the path. See above
 	})
 	.listen(3080);
