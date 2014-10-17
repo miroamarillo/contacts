1149202331df9b7968ff6f790b23e4eb45386c5c
Julian Pineda <studio@miroamarillo.com>
2014-10-17 11:44:25 -0400

    Controller for the Settings Options

--- public/src/controllers.js
+++ public/src/controllers.js
@@ -57,4 +57,27 @@
 					$location.url('/contacts');
 				}
 		}])
+		.controller('SettingsController', ['$scope', '$rootScope', 'options', 'Fields', function($scope, $rootScope, options, Fields){
+
+			$rootScope.PAGE = 'settings';
+
+			$scope.allFields = [];
+			$scope.fields = options.displayed_fields;
+
+			Fields.headers().then(function (data) {
+				$scope.allFields = data;
+			});
+
+			$scope.toggle = function (field) {
+				var i = options.displayed_fields.indexOf(field);
+
+				if (i > -1) {
+					options.displayed_fields.splice(i, 1);
+				} else {
+					options.displayed_fields.push(field);
+				}
+
+				Fields.set(options.displayed_fields);
+			};
+		}])
 })();
